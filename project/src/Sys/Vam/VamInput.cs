0486949b85040116fb5bd43222502b5f2a14dcbe
via5 <65577279+via5@users.noreply.github.com>
2021-04-27 01:10:44 -0400

    split

---
+++ project/src/Sys/Vam/VamInput.cs
@@ -0,0 +1,228 @@
+﻿using UnityEngine;
+
+namespace Cue.W
+{
+	class VamInput : IInput
+	{
+		private VamSys sys_;
+		private Ray ray_ = new Ray();
+		private bool controlsToggle_ = false;
+		private Vector3 middlePos_ = Vector3.Zero;
+		private bool middleDown_ = false;
+
+		public VamInput(VamSys sys)
+		{
+			sys_ = sys;
+		}
+
+		public bool ReloadPlugin
+		{
+			get
+			{
+				return Input.GetKeyDown(KeyCode.F5);
+			}
+		}
+
+		public bool MenuToggle
+		{
+			get
+			{
+				if (sys_.IsVR)
+					return SuperController.singleton.GetLeftSelect();
+				else
+					return false;
+			}
+		}
+
+		public bool Select
+		{
+			get
+			{
+				if (sys_.IsVR)
+					return SuperController.singleton.GetRightSelect();
+				else
+					return Input.GetMouseButtonUp(0);
+			}
+		}
+
+		public bool Action
+		{
+			get
+			{
+				if (sys_.IsVR)
+					return SuperController.singleton.GetRightGrab();
+				else
+					return Input.GetMouseButtonUp(1);
+			}
+		}
+
+		public bool ShowControls
+		{
+			get
+			{
+				if (sys_.IsVR)
+				{
+					return
+						SuperController.singleton.GetLeftUIPointerShow() ||
+						SuperController.singleton.GetRightUIPointerShow();
+				}
+				else
+				{
+					if (middleDown_)
+					{
+						if (Input.GetMouseButtonUp(2))
+						{
+							var cp = Vector3.FromUnity(
+								SuperController.singleton.MonitorCenterCamera
+									.transform.position);
+
+							var d = Vector3.Distance(middlePos_, cp);
+							if (d < 0.02f)
+								controlsToggle_ = !controlsToggle_;
+
+							middleDown_ = false;
+						}
+					}
+					else
+					{
+						if (Input.GetMouseButtonDown(2))
+						{
+							var cp = Vector3.FromUnity(
+								SuperController.singleton.MonitorCenterCamera
+									.transform.position);
+
+							middleDown_ = true;
+							middlePos_ = cp;
+						}
+					}
+
+					return controlsToggle_;
+				}
+			}
+		}
+
+		public IObject GetHovered()
+		{
+			if (sys_.IsVR)
+			{
+				if (!GetVRRay())
+					return null;
+			}
+			else
+			{
+				if (!GetMouseRay())
+					return null;
+			}
+
+			if (HitUI())
+				return null;
+
+			var o = HitObject();
+			if (o != null)
+				return o;
+
+			o = HitPerson();
+			if (o != null)
+				return o;
+
+			return null;
+		}
+
+		private bool GetMouseRay()
+		{
+			ray_ = SuperController.singleton.MonitorCenterCamera
+				.ScreenPointToRay(Input.mousePosition);
+
+			return true;
+		}
+
+		private bool GetVRRay()
+		{
+			if (SuperController.singleton.GetLeftUIPointerShow())
+			{
+				ray_.origin = SuperController.singleton.viveObjectLeft.position;
+				ray_.direction = SuperController.singleton.viveObjectLeft.forward;
+				return true;
+			}
+			else if (SuperController.singleton.GetRightUIPointerShow())
+			{
+				ray_.origin = SuperController.singleton.viveObjectRight.position;
+				ray_.direction = SuperController.singleton.viveObjectRight.forward;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private bool HitUI()
+		{
+			// todo
+			return false;
+		}
+
+		private IObject HitObject()
+		{
+			RaycastHit hit;
+
+			bool b = Physics.Raycast(
+				ray_, out hit, float.MaxValue, 1 << Controls.Layer);
+
+			if (!b)
+				return null;
+
+			return Cue.Instance.Controls.Find(hit.transform);
+		}
+
+		private IObject HitPerson()
+		{
+			var a = HitAtom();
+			if (a == null)
+				return null;
+
+			var ps = Cue.Instance.Persons;
+
+			for (int i = 0; i < ps.Count; ++i)
+			{
+				if (((W.VamAtom)ps[i].Atom).Atom == a)
+					return ps[i];
+			}
+
+			return null;
+		}
+
+		private Atom HitAtom()
+		{
+			RaycastHit hit;
+			bool b = Physics.Raycast(
+				ray_, out hit, float.MaxValue, 0x24000100);
+
+			if (!b)
+				return null;
+
+			var fc = hit.transform.GetComponent<FreeControllerV3>();
+
+			if (fc != null)
+				return fc.containingAtom;
+
+			var bone = hit.transform.GetComponent<DAZBone>();
+			if (bone != null)
+				return bone.containingAtom;
+
+			var rb = hit.transform.GetComponent<Rigidbody>();
+			var p = rb.transform;
+
+			while (p != null)
+			{
+				var a = p.GetComponent<Atom>();
+				if (a != null)
+					return a;
+
+				p = p.parent;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ project/src/Sys/Vam/VamLog.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+namespace Cue.W
+{
+	class VamLog : ILog
+	{
+		private UnityEngine.UI.Button clear_ = null;
+		private UnityEngine.UI.Text logText_ = null;
+		private UIStyleText logStyle_ = null;
+		private int logFontSize_ = -1;
+		private Font logFont_ = null;
+
+		public VamLog()
+		{
+		}
+
+		public void Clear()
+		{
+			if (clear_ == null)
+			{
+				var t = VUI.Utilities.FindChildRecursive(
+					SuperController.singleton.errorLogPanel, "ClearButton");
+
+				clear_ = t?.GetComponent<UnityEngine.UI.Button>();
+			}
+
+			clear_?.onClick?.Invoke();
+		}
+
+		public void Verbose(string s)
+		{
+			SuperController.LogError("  [V] " + s);
+		}
+
+		public void Info(string s)
+		{
+			SuperController.LogError("  [I] " + s);
+		}
+
+		public void Error(string s)
+		{
+			SuperController.LogError("!![E] " + s);
+		}
+
+		public void OnPluginState(bool b)
+		{
+			if (logText_ == null)
+			{
+				var vp = VUI.Utilities.FindChildRecursive(
+					SuperController.singleton.errorLogPanel, "Viewport");
+
+				var textObject = VUI.Utilities.FindChildRecursive(vp, "Text");
+
+				logText_ = textObject.GetComponent<UnityEngine.UI.Text>();
+				logStyle_ = textObject.GetComponent<UIStyleText>();
+			}
+
+			if (b)
+			{
+				logFontSize_ = logStyle_.fontSize;
+				logFont_ = logText_.font;
+				logStyle_.fontSize = 24;
+				logText_.resizeTextForBestFit = false;
+				logText_.font = Font.CreateDynamicFontFromOSFont("Consolas", 24);
+				logStyle_.UpdateStyle();
+			}
+			else
+			{
+				logStyle_.fontSize = logFontSize_;
+				logText_.fontSize = logFontSize_;
+				logText_.font = logFont_;
+				logText_.resizeTextForBestFit = true;
+				logStyle_.UpdateStyle();
+			}
+		}
+	}
+}
