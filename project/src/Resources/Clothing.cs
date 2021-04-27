82b8ac1fdee9155e68fb0c563e36ba3824b87645
via5 <65577279+via5@users.noreply.github.com>
2021-04-27 02:26:02 -0400

    split resources

---
+++ project/src/Resources/Clothing.cs
@@ -0,0 +1,169 @@
+﻿using SimpleJSON;
+using System;
+using System.Collections.Generic;
+
+namespace Cue.Resources
+{
+	class Clothing
+	{
+		public class Item
+		{
+			public string what;
+			public int sex;
+
+			public bool showsGenitalsBool = false;
+			public bool hidesGenitalsBool = false;
+			public string showsGenitalsState = "";
+			public string hidesGenitalsState = "";
+
+			public bool showsBreastsBool = false;
+			public bool hidesBreastsBool = false;
+			public string showsBreastsState = "";
+			public string hidesBreastsState = "";
+
+			public Item(string what, int sex)
+			{
+				this.what = what;
+				this.sex = sex;
+			}
+
+			public override string ToString()
+			{
+				string s = what + " sex=" + Sexes.ToString(sex) + " ";
+
+				if (showsGenitalsState == "")
+					s += $"showsGenitals={showsGenitalsBool} ";
+				else
+					s += $"showsGenitals={showsGenitalsState} ";
+
+				if (hidesGenitalsState == "")
+					s += $"hidesGenitals={hidesGenitalsBool} ";
+				else
+					s += $"hidesGenitals={hidesGenitalsState} ";
+
+				if (showsBreastsState == "")
+					s += $"showsBreasts={showsBreastsBool} ";
+				else
+					s += $"showsBreasts={showsBreastsState} ";
+
+				if (hidesBreastsState == "")
+					s += $"hidesBreasts={hidesBreastsBool} ";
+				else
+					s += $"hidesBreasts={hidesBreastsState} ";
+
+				return s;
+			}
+		}
+
+		private static Dictionary<string, Item> ids_ =
+			new Dictionary<string, Item>();
+
+		private static Dictionary<string, Item> tags_ =
+			new Dictionary<string, Item>();
+
+		public static bool Load()
+		{
+			try
+			{
+				DoLoad();
+				return true;
+			}
+			catch (Exception e)
+			{
+				Cue.LogError("failed to load clothing, " + e.Message);
+				return false;
+			}
+		}
+
+		private static void DoLoad()
+		{
+			var meta = Cue.Instance.Sys.GetResourcePath("clothing.json");
+			var doc = JSON.Parse(Cue.Instance.Sys.ReadFileIntoString(meta));
+
+			if (doc == null)
+			{
+				Cue.LogError("failed to parse clothing");
+				return;
+			}
+
+			foreach (var an in doc.AsObject["clothing"].AsArray.Childs)
+			{
+				var a = an.AsObject;
+				int sex = Sexes.Any;
+				string id = "", tag = "";
+
+				if (a.HasKey("id"))
+				{
+					id = a["id"].Value;
+				}
+				else if (a.HasKey("tag"))
+				{
+					tag = a["tag"].Value;
+				}
+				else
+				{
+					Cue.LogError("clothing item missing id or tag");
+					continue;
+				}
+
+
+				if (a.HasKey("sex"))
+					sex = Sexes.FromString(a["sex"].Value);
+
+				var item = new Item(id != "" ? id : tag, sex);
+
+				if (a.HasKey("showsGenitals"))
+					item.showsGenitalsBool = a["showsGenitals"].AsBool;
+				else if (a.HasKey("showsGenitalsState"))
+					item.showsGenitalsState = a["showsGenitalsState"];
+
+				if (a.HasKey("hidesGenitals"))
+					item.hidesGenitalsBool = a["hidesGenitals"].AsBool;
+				else if (a.HasKey("hidesGenitalsState"))
+					item.hidesGenitalsState = a["hidesGenitalsState"];
+
+				if (a.HasKey("showsBreasts"))
+					item.showsBreastsBool = a["showsBreasts"].AsBool;
+				else if (a.HasKey("showsBreastsState"))
+					item.showsBreastsState = a["showsBreastsState"];
+
+				if (a.HasKey("hidesBreasts"))
+					item.hidesBreastsBool = a["hidesBreasts"].AsBool;
+				else if (a.HasKey("hidesBreastsState"))
+					item.hidesBreastsState = a["hidesBreastsState"];
+
+				Cue.LogInfo("clothing item: " + item.ToString());
+
+				if (id != "")
+					ids_.Add(id, item);
+				else
+					tags_.Add(tag, item);
+			}
+		}
+
+		public static Item FindItem(int sex, string id, string[] tags)
+		{
+			Item item;
+
+			if (ids_.TryGetValue(id, out item))
+			{
+				if (Sexes.Match(sex, item.sex))
+					return item;
+			}
+
+			if (tags != null)
+			{
+				for (int i = 0; i < tags.Length; ++i)
+				{
+					if (tags_.TryGetValue(tags[i], out item))
+					{
+						if (Sexes.Match(item.sex, sex))
+							return item;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+}
