039cae8bfa94631a7124205caabf126c3f3dab97
via5 <65577279+via5@users.noreply.github.com>
2021-05-09 00:01:33 -0400

    logs in script ui

--- project/src/UI/ScriptUI.cs
+++ project/src/UI/ScriptUI.cs
@@ -69,8 +69,30 @@ namespace Cue
 		private VUI.Label objects_ = new VUI.Label();
 		private VUI.Label ui_ = new VUI.Label();
 
+		private VUI.CheckBox logAnimation_;
+		private VUI.CheckBox logAction_;
+		private VUI.CheckBox logInteraction_;
+		private VUI.CheckBox logAI_;
+		private VUI.CheckBox logEvent_;
+		private VUI.CheckBox logIntegration_;
+		private VUI.CheckBox logObject_;
+		private VUI.CheckBox logSlots_;
+		private VUI.CheckBox logSys_;
+		private VUI.CheckBox logClothing_;
+
 		public MiscTab()
 		{
+			logAnimation_ = new VUI.CheckBox("Animation", CheckLog);
+			logAction_ = new VUI.CheckBox("Action", CheckLog);
+			logInteraction_ = new VUI.CheckBox("Interaction", CheckLog);
+			logAI_ = new VUI.CheckBox("AI", CheckLog);
+			logEvent_ = new VUI.CheckBox("Event", CheckLog);
+			logIntegration_ = new VUI.CheckBox("Integration", CheckLog);
+			logObject_ = new VUI.CheckBox("Object", CheckLog);
+			logSlots_ = new VUI.CheckBox("Slots", CheckLog);
+			logSys_ = new VUI.CheckBox("Sys", CheckLog);
+			logClothing_ = new VUI.CheckBox("Clothing", CheckLog);
+
 			Layout = new VUI.VerticalFlow();
 			Add(navmeshes_);
 			Add(renav_);
@@ -97,6 +119,19 @@ namespace Cue
 			p.Add(fixedUpdate_);
 
 			Add(p);
+			Add(new VUI.Spacer(30));
+
+			Add(new VUI.Label("Logs", UnityEngine.FontStyle.Bold));
+			Add(logAnimation_);
+			Add(logAction_);
+			Add(logInteraction_);
+			Add(logAI_);
+			Add(logEvent_);
+			Add(logIntegration_);
+			Add(logObject_);
+			Add(logSlots_);
+			Add(logSys_);
+			Add(logClothing_);
 
 			navmeshes_.Changed += (b) => Cue.Instance.Sys.Nav.Render = b;
 			renav_.Clicked += Cue.Instance.Sys.Nav.Update;
@@ -121,6 +156,24 @@ namespace Cue
 				ui_.Text = tickers.ui.ToString();
 				fixedUpdate_.Text = tickers.fixedUpdate.ToString();
 			}
+		}
+
+		private void CheckLog(bool b)
+		{
+			int e = 0;
+
+			if (logAnimation_.Checked) e |= Logger.Animation;
+			if (logAction_.Checked) e |= Logger.Action;
+			if (logInteraction_.Checked) e |= Logger.Interaction;
+			if (logAI_.Checked) e |= Logger.AI;
+			if (logEvent_.Checked) e |= Logger.Event;
+			if (logIntegration_.Checked) e |= Logger.Integration;
+			if (logObject_.Checked) e |= Logger.Object;
+			if (logSlots_.Checked) e |= Logger.Slots;
+			if (logSys_.Checked) e |= Logger.Sys;
+			if (logClothing_.Checked) e |= Logger.Clothing;
+
+			Logger.Enabled = e;
 		}
 	}
 
