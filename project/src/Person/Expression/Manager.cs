bb555d4881e34bbe33ef08bec7be79f99e16ee9f
via5 <65577279+via5@users.noreply.github.com>
2021-11-01 10:49:55 -0400

    split

---
+++ project/src/Person/Expression/Manager.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cue
+{
+	class ExpressionManager
+	{
+		private const int MaxActive = 4;
+		private const float MoreCheckInterval = 1;
+
+		private Person person_;
+		private WeightedExpression[] exps_ = new WeightedExpression[0];
+		private bool needsMore_ = false;
+		private float moreElapsed_ = 0;
+		private Personality lastPersonality_ = null;
+		private bool enabled_ = true;
+		private bool isOrgasming_ = false;
+
+		public ExpressionManager(Person p)
+		{
+			person_ = p;
+		}
+
+		public Expression[] GetExpressionsForMood(int mood)
+		{
+			var list = new List<Expression>();
+
+			for (int i = 0; i < exps_.Length; ++i)
+			{
+				if (exps_[i].Expression.IsMood(mood))
+					list.Add(exps_[i].Expression);
+			}
+
+			return list.ToArray();
+		}
+
+		public void Enable()
+		{
+			for (int i = 0; i < exps_.Length; ++i)
+				exps_[i].Deactivate();
+
+			enabled_ = true;
+
+			for (int i = 0; i < MaxActive; ++i)
+				NextActive();
+		}
+
+		public void Disable()
+		{
+			for (int i = 0; i < exps_.Length; ++i)
+				exps_[i].Deactivate();
+
+			enabled_ = false;
+		}
+
+		private void Init()
+		{
+			var all = person_.Personality.GetExpressions();
+			exps_ = new WeightedExpression[all.Length];
+
+			for (int i = 0; i < all.Length; ++i)
+			{
+				all[i].Init(person_);
+				exps_[i] = new WeightedExpression(all[i]);
+			}
+
+			for (int i = 0; i < MaxActive; ++i)
+				NextActive();
+
+			lastPersonality_ = person_.Personality;
+		}
+
+		public void FixedUpdate(float s)
+		{
+			if (lastPersonality_ != person_.Personality)
+				Init();
+
+
+			if (!isOrgasming_ && person_.Mood.State == Mood.OrgasmState)
+			{
+				isOrgasming_ = true;
+				for (int i = 0; i < exps_.Length; ++i)
+					exps_[i].Deactivate();
+			}
+			else if (isOrgasming_ && person_.Mood.State != Mood.OrgasmState)
+			{
+				isOrgasming_ = false;
+			}
+
+
+			int finished = 0;
+			int activeCount = 0;
+
+			for (int i = 0; i < exps_.Length; ++i)
+			{
+				exps_[i].FixedUpdate(s);
+
+				if (exps_[i].Active)
+				{
+					if (exps_[i].Finished)
+					{
+						exps_[i].Deactivate();
+						++finished;
+					}
+					else
+					{
+						++activeCount;
+					}
+				}
+			}
+
+			if (!enabled_)
+				return;
+
+			for (int i = 0; i < finished; ++i)
+			{
+				if (NextActive())
+					++activeCount;
+			}
+
+			if (activeCount < MaxActive)
+			{
+				if (needsMore_)
+				{
+					moreElapsed_ += s;
+
+					if (moreElapsed_ > MoreCheckInterval)
+					{
+						moreElapsed_ = 0;
+						var tries = MaxActive - activeCount;
+
+						for (int i = 0; i < tries; ++i)
+						{
+							if (NextActive())
+								++activeCount;
+						}
+
+						if (activeCount >= MaxActive)
+							needsMore_ = false;
+					}
+				}
+				else
+				{
+					needsMore_ = true;
+					moreElapsed_ = 0;
+				}
+			}
+		}
+
+		private bool NextActive()
+		{
+			UpdateExpressions();
+
+			float totalWeight = 0;
+			for (int i = 0; i < exps_.Length; ++i)
+			{
+				if (exps_[i].Active)
+					continue;
+
+				totalWeight += exps_[i].Weight;
+			}
+
+			if (totalWeight > 0)
+			{
+				var r = U.RandomFloat(0, totalWeight);
+
+				for (int i = 0; i < exps_.Length; ++i)
+				{
+					if (exps_[i].Active)
+						continue;
+
+					if (r < exps_[i].Weight)
+					{
+						exps_[i].Activate();
+						return true;
+					}
+
+					r -= exps_[i].Weight;
+				}
+			}
+
+			return false;
+		}
+
+		private void UpdateExpressions()
+		{
+			var m = person_.Mood;
+			var ps = person_.Personality;
+
+			float expressionTiredness = U.Clamp(
+				m.Get(Moods.Tired) * ps.Get(PSE.ExpressionTirednessFactor),
+				0, 1);
+
+
+			for (int i = 0; i < exps_.Length; ++i)
+			{
+				var we = exps_[i];
+				var e = we.Expression;
+
+				float weight = 0;
+				float intensity = 0;
+
+
+				if (isOrgasming_)
+				{
+					if (e.IsMood(Moods.Orgasm))
+					{
+						weight += 1;
+						intensity = 1;
+					}
+				}
+				else
+				{
+					if (e.IsMood(Moods.Happy))
+					{
+						weight += m.Get(Moods.Happy);
+						intensity = Math.Max(intensity, m.Get(Moods.Happy));
+					}
+
+					if (e.IsMood(Moods.Excited))
+					{
+						weight += m.Get(Moods.Excited) * 2;
+						intensity = Math.Max(intensity, m.Get(Moods.Excited));
+					}
+
+					if (e.IsMood(Moods.Angry))
+					{
+						weight += m.Get(Moods.Angry);
+						intensity = Math.Max(intensity, m.Get(Moods.Angry));
+					}
+
+					if (e.IsMood(Moods.Tired))
+					{
+						weight += expressionTiredness;
+						intensity = Math.Max(intensity, expressionTiredness);
+					}
+				}
+
+
+				if (!e.IsMood(Moods.Tired))
+				{
+					weight *= Math.Max(1 - expressionTiredness, 0.05f);
+				}
+
+
+				float speed = 1 - expressionTiredness;
+
+				we.Set(weight, intensity, speed);
+			}
+		}
+
+		public void OnPluginState(bool b)
+		{
+			if (!b)
+			{
+				for (int i = 0; i < exps_.Length; ++i)
+					exps_[i].Reset();
+			}
+		}
+
+		public string[] Debug()
+		{
+			var s = new string[MaxActive + 1 + exps_.Length + 2];
+
+			int i = 0;
+
+			for (int j = 0; j < exps_.Length; ++j)
+			{
+				if (exps_[j].Active)
+					s[i++] = $"{exps_[j].ToDetailedString()}";
+			}
+
+			while (i < MaxActive)
+				s[i++] = "none";
+
+			s[i++] = "";
+
+			for (int j = 0; j < exps_.Length; ++j)
+				s[i++] = $"{exps_[j]}";
+
+			s[i++] = "";
+			s[i++] = (needsMore_ ? "needs more" : "");
+
+			return s;
+		}
+	}
+}
---
+++ project/src/Person/Expression/WeightedExpression.cs
@@ -0,0 +1,171 @@
+﻿namespace Cue
+{
+	class WeightedExpression
+	{
+		private float FastTimeMin = 0.4f;
+		private float FastTimeMax = 2.0f;
+		private float SlowTimeMin = 3.0f;
+		private float SlowTimeMax = 5.0f;
+
+		private const int InactiveState = 0;
+		private const int RampUpState = 1;
+		private const int HoldState = 2;
+		private const int FinishedState = 3;
+
+		private readonly Expression e_;
+		private int state_ = InactiveState;
+
+		private float weight_ = 0;
+		private float intensity_ = 0;
+		private float speed_ = 0;
+
+		private Duration holdTime_ = new Duration(0, 3);
+
+
+		public WeightedExpression(Expression e)
+		{
+			e_ = e;
+			Deactivate();
+		}
+
+		public Expression Expression
+		{
+			get { return e_; }
+		}
+
+		public float Weight
+		{
+			get { return weight_; }
+		}
+
+		public float Intensity
+		{
+			get { return intensity_; }
+		}
+
+		public float Speed
+		{
+			get { return speed_; }
+		}
+
+		public void Set(float weight, float intensity, float speed)
+		{
+			weight_ = weight;
+			intensity_ = intensity;
+			speed_ = speed;
+		}
+
+		public bool Active
+		{
+			get { return (state_ != InactiveState); }
+		}
+
+		public bool Finished
+		{
+			get { return (state_ == FinishedState); }
+		}
+
+		public void Activate()
+		{
+			Activate(RandomTarget(), RandomTargetTime());
+		}
+
+		public void Activate(float target, float time)
+		{
+			e_.SetAuto(0.1f, MinRandomTime(), MaxRandomTime());
+			e_.SetTarget(target, time);
+			state_ = RampUpState;
+		}
+
+		public void Deactivate()
+		{
+			e_.SetAuto(0, 0, 0);
+			e_.SetTarget(0, RandomResetTime());
+			state_ = InactiveState;
+		}
+
+		public void FixedUpdate(float s)
+		{
+			e_.FixedUpdate(s);
+
+			switch (state_)
+			{
+				case InactiveState:
+				{
+					break;
+				}
+
+				case RampUpState:
+				{
+					if (e_.Finished)
+						state_ = HoldState;
+
+					break;
+				}
+
+				case HoldState:
+				{
+					holdTime_.Update(s);
+					if (holdTime_.Finished)
+						state_ = FinishedState;
+
+					break;
+				}
+			}
+		}
+
+		public void Reset()
+		{
+			e_.Reset();
+		}
+
+		public string ToDetailedString()
+		{
+			return
+				$"{e_} w={weight_:0.00} i={intensity_:0.00} " +
+				$"s={speed_:0.00} {StateToString(state_)}";
+		}
+
+		private static string StateToString(int state)
+		{
+			switch (state)
+			{
+				case InactiveState: return "inactive";
+				case RampUpState: return "rampup";
+				case HoldState: return "hold";
+				case FinishedState: return "finished";
+				default: return $"?{state}";
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{e_.Name} ({e_.MoodString()}) w={weight_:0.00}";
+		}
+
+		private float RandomTarget()
+		{
+			return U.RandomFloat(0, 1) * U.Clamp(intensity_, 0, 1);
+		}
+
+		private float MinRandomTime()
+		{
+			return FastTimeMin + (SlowTimeMin - FastTimeMin) * (1 - speed_);
+		}
+
+		private float MaxRandomTime()
+		{
+			return FastTimeMax + (SlowTimeMax - FastTimeMax) * (1 - speed_);
+		}
+
+		private float RandomTargetTime()
+		{
+			return U.RandomFloat(MinRandomTime(), MaxRandomTime());
+		}
+
+		private float RandomResetTime()
+		{
+			return RandomTargetTime();
+		}
+	}
+}
