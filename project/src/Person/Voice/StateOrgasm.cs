59d7c56cc6fc3e7a5322b27d3c4eb10a220fcef2
via5 <65577279+via5@users.noreply.github.com>
2022-06-23 00:43:58 -0400

    added voice option, fixes bored

--- project/src/Person/Voice/StateOrgasm.cs
+++ project/src/Person/Voice/StateOrgasm.cs
@@ -4,6 +4,12 @@ namespace Cue
 {
 	public class VoiceStateOrgasm : VoiceState
 	{
+		private const int OrgasmAction = 1;
+		private const int MoaningAction = 2;
+
+		private int action_ = OrgasmAction;
+		private float moaningIntensity_ = 0;
+
 		private VoiceStateOrgasm()
 		{
 		}
@@ -17,6 +23,26 @@ namespace Cue
 		{
 			if (vo.HasKey("orgasmState"))
 			{
+				var o = vo["orgasmState"].AsObject;
+
+				string v = J.OptString(o, "voice", "");
+				if (v == "orgasm")
+				{
+					action_ = OrgasmAction;
+				}
+				else if (v == "moaning")
+				{
+					action_ = MoaningAction;
+
+					if (o.HasKey("moaningIntensity"))
+						moaningIntensity_ = J.ReqFloat(o, "moaningIntensity");
+					else if (!inherited)
+						throw new LoadFailed("missing moaningIntensity");
+				}
+				else if (v != "")
+				{
+					throw new LoadFailed("bad orgasmState voice, must be 'orgasm' or 'moaning'");
+				}
 			}
 			else if (!inherited)
 			{
@@ -38,11 +64,16 @@ namespace Cue
 
 		private void CopyFrom(VoiceStateOrgasm o)
 		{
+			action_ = o.action_;
+			moaningIntensity_ = o.moaningIntensity_;
 		}
 
 		protected override void DoStart()
 		{
-			v_.Provider.SetOrgasm();
+			if (action_ == OrgasmAction)
+				v_.Provider.SetOrgasm();
+			else if (action_ == MoaningAction)
+				v_.Provider.SetMoaning(moaningIntensity_);
 		}
 
 		protected override void DoUpdate(float s)
@@ -76,6 +107,8 @@ namespace Cue
 
 		protected override void DoDebug(DebugLines debug)
 		{
+			debug.Add("action", (action_ == OrgasmAction ? "orgasm" : "moaning"));
+			debug.Add("moaning", $"{moaningIntensity_:0.00}");
 		}
 	}
 }
