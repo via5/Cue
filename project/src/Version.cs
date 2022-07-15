1c0954022c1c1a4d90babf3a4d830976da6c50dd
via5 <65577279+via5@users.noreply.github.com>
2022-07-14 20:08:01 -0400

    bumped to 2.0

--- project/src/Version.cs
+++ project/src/Version.cs
@@ -2,8 +2,8 @@
 {
 	class Version
 	{
-		public static readonly int Major = 1;
-		public static readonly int Minor = 1;
+		public static readonly int Major = 2;
+		public static readonly int Minor = 0;
 		public static readonly int Patch = 0;
 
 		public static string String
