b161ec79e9fde02c38103ea74a57c8f9296725b7
via5 <65577279+via5@users.noreply.github.com>
2023-02-14 10:40:14 -0500

    bumped to 3.1

--- project/src/Version.cs
+++ project/src/Version.cs
@@ -3,7 +3,7 @@
 	class Version
 	{
 		public static readonly int Major = 3;
-		public static readonly int Minor = 0;
+		public static readonly int Minor = 1;
 		public static readonly int Patch = 0;
 
 		public static string String
